e4255f9c490da6d5f3d5760b713e3f9162376ab6
mgoltzsche <mgoltzsche@users.noreply.github.com>
2024-09-07 02:37:19 +0000

    chore(release): 0.5.0 [skip ci]
    ## v0.5.0 (2024-09-07)
    
    ### Features
    
    - add FastAPI support, run with uvicorn server (e853408)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='beets-webrouter',
-    version='0.0.0-dev',
+    version='0.5.0',
     author='Max Goltzsche',
     description='Serve multiple beets APIs on the same IP/port using a single command',
     long_description=long_description,
